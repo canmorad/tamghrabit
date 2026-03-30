1e3be64747278d1d298edae1b32095300574f20d
Morad Benaissa <youcanmorad@gmail.com>
2026-03-30 03:32:39 +0100

    feat: ajout du front-end de la page Explorer avec filtres et grille de campagnes

--- app/Controllers/CampagneController.php
+++ app/Controllers/CampagneController.php
@@ -5,13 +5,18 @@ use App\Core\Controller;
 use App\Helpers\Session;
 class CampagneController extends Controller
 {
+    public function index()
+    {
+        return $this->view("explorer/campagnes");
+    }
     public function create()
     {
         return $this->view("campagne/create");
     }
 
-    public function store(){
-      
+    public function store()
+    {
+
 
     }
 }
